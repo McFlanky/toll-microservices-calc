a1d922ae5dee671be7a33a2a1399e4482879a784
McFlanky <cturjanica02@gmail.com>
2024-05-13 12:32:29 -0400

    created main.go OBU for generating random simulated cooridinates and OBUIDs from 0-100 + 1 and using gorilla for websocket connection communication over TCP so far

---
+++ obu/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+const wsEndpoint = "ws://127.0.0.1:30000/ws"
+
+var sendInterval = time.Second
+
+type OBUData struct {
+	OBUID int     `json:"obuID"`
+	Lat   float64 `json:"lat"`
+	Long  float64 `json:"long"`
+}
+
+func genLatLong() (float64, float64) {
+	return genCoordinate(), genCoordinate()
+}
+
+func genCoordinate() float64 {
+	n := float64(rand.Intn(100) + 1)
+	f := rand.Float64()
+	return n + f
+}
+
+func main() {
+	obuIDs := genOBUIDs(20)
+	conn, _, err := websocket.DefaultDialer.Dial(wsEndpoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		for i := 0; i < len(obuIDs); i++ {
+			lat, long := genLatLong()
+			data := OBUData{
+				OBUID: obuIDs[i],
+				Lat:   lat,
+				Long:  long,
+			}
+			if err := conn.WriteJSON(data); err != nil {
+				log.Fatal(err)
+			}
+		}
+		time.Sleep(sendInterval)
+	}
+}
+
+func genOBUIDs(n int) []int {
+	ids := make([]int, n)
+	for i := 0; i < n; i++ {
+		ids[i] = rand.Intn(math.MaxInt)
+	}
+	return ids
+}
+
+func init() {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+}
